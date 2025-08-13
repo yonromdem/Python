7586af47dfc6fd6f302e9617672ad0aa8b14db2f
Yonas Bekele <yonromdem@gmail.com>
2025-08-13 14:55:44 +0300

    draw a green circle at mouse position

---
+++ drawcircle.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+
+screen= pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Draw Circle at mouse position")
+
+WHT, BLU, RED, BLK = (255,255,255), (0,200,255), (255,0,0), (0,0,0)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(BLK)
+    
+
+    if pygame.mouse.get_pressed()[0]:  # Check if the left mouse button is pressed
+        pos = pygame.mouse.get_pos()
+        # Uncomment the line below to draw a circle at the mouse position
+        pygame.draw.circle(screen, (0, 255, 0), pos, 20)  
+
+    pygame.display.flip()
