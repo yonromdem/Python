dc05fa2ed3dcd69134d8339de3bcc775fddc2144
Yonas Bekele <yonromdem@gmail.com>
2025-08-13 14:21:14 +0300

    Add initial implementation of C/C++ build tasks and Python game

---
+++ C/cex01.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/*int main (){
+printf("Welcome \\ Burgerface\n\n\n");
+printf("Hello World");
+return 0;
+}*/
+int main (){
+int myNum= 15;
+int myByear=2005;
+printf("%d\n",myNum);
+printf("%d",myByear);
+return 0;
+}
---
+++ C/cex02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int myFavn0=1982;
+int myByear=2005;
+printf("My favourite Number is %d and mybirthyear is %d ", myFavn0,myByear);
+float price=5.15;
+char letter='A';
+printf("The burgerface is %f and the cookie %c", price, letter);
+return 0;
+}
